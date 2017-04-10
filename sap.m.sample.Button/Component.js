57a2e6aa75c23d1d6d9aa1b46d1d65d4bfb769df
shyam kumar <scrapshyam@gmail.com>
2017-04-10 10:47:20 +0000

    test new file

--- sap.m.sample.Button/Component.js
+++ sap.m.sample.Button/Component.js
@@ -3,7 +3,6 @@ sap.ui.define(['sap/ui/core/UIComponent'],
 	"use strict";
 //new comment added by Shyam
 	//Check backward Pull request functionality
-
 	//changed line by Shyam - by me
 
 	var Component = UIComponent.extend("sap.m.sample.Button.Component", {
