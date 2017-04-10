a036b40b6afe06b444f8c812eb81d278347e14c8
shyamkumarc <scrapshyam@gmail.com>
2017-04-10 13:04:51 +0530

    Update Component.js
    update from github page

--- sap.m.sample.Button/Component.js
+++ sap.m.sample.Button/Component.js
@@ -2,6 +2,8 @@ sap.ui.define(['sap/ui/core/UIComponent'],
 	function(UIComponent) {
 	"use strict";
 //new comment added by Shyam
+	//Check backward Pull request functionality
+	
 	var Component = UIComponent.extend("sap.m.sample.Button.Component", {
 
 		metadata : {
