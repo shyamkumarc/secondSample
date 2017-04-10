0280fe8e3ffcec9e7c5e2ee84e2cf84c9601cc74
Gautam Malla <malla.gautam@yahoo.com>
2017-04-10 10:34:01 +0000

    gauthams cahnges

--- sap.m.sample.Button/Page.controller.js
+++ sap.m.sample.Button/Page.controller.js
@@ -3,7 +3,7 @@ sap.ui.define(['sap/m/MessageToast','sap/ui/core/mvc/Controller'],
 	"use strict";
 
 	var PageController = Controller.extend("sap.m.sample.Button.Page", {
-
+// cahnged line by gautham
 		onPress: function (evt) {
 			MessageToast.show(evt.getSource().getId() + " Pressed");
 		}
